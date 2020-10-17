2639c0d05e8289d7c2b03b8b2d3521fce0b0e37a
Louise Cerdeira <lcerdeira@gmail.com>
2020-10-17 20:34:09 +1100

    Updates

---
+++ precache-manifest.7f05d26f14353c3e42948b0219ea0258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334cb446aceaf5bd0d0107370ba5f079",
+    "url": "/empdir/index.html"
+  },
+  {
+    "revision": "2f82529a3bcda4551355",
+    "url": "/empdir/static/css/main.35d4597b.chunk.css"
+  },
+  {
+    "revision": "21bd0cfac32b3a54f5eb",
+    "url": "/empdir/static/js/2.0fa9503b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/empdir/static/js/2.0fa9503b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f82529a3bcda4551355",
+    "url": "/empdir/static/js/main.c675afaa.chunk.js"
+  },
+  {
+    "revision": "1413c6472883011c2243",
+    "url": "/empdir/static/js/runtime-main.cbe69b73.js"
+  }
+]);
